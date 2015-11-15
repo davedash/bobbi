9795383dfcc060cde178ea887a80f921f99adac8
Dave Dash <dd@davedash.com>
2015-11-14 23:39:34 -0800

    Read from a specified config file.

--- main.go
+++ main.go
@@ -2,12 +2,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gopkg.in/gcfg.v1"
 	"log"
 	"net/http"
-	"os"
 )
+
+var configFile = flag.String("config", "/etc/bobbi.conf", "INI file for bobbi")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
@@ -19,27 +21,27 @@ type config struct {
 	}
 }
 
-func getConfig() config {
+func getConfig(filename string) config {
 	cfg := new(config)
 
-	err := gcfg.ReadFileInto(cfg, "./config-example.conf")
+	err := gcfg.ReadFileInto(cfg, filename)
 
 	if err != nil {
-		log.Fatalf("Failed to parse gcfg data: %s", err)
-		os.Exit(1)
+		log.Fatalf("Failed reading config: %s", err)
 	}
 
 	return *cfg
 }
 
 func main() {
-	cfg := getConfig()
+	flag.Parse()
+	cfg := getConfig(*configFile)
 	port := cfg.Main.Port
 	if port == "" {
 		port = "8000"
 	}
 
-	fmt.Println(port)
+	fmt.Println("Listening on port:", port)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
